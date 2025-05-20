47d3e00b3d060dd2accc8b376fa53869a1486832
Mateus de Oliveira <mateusoliveira9176@gmail.com>
2025-05-19 21:51:30 -0300

    att

--- app/src/main/java/com/example/projetowarzone/MultiplayerFragment.java
+++ app/src/main/java/com/example/projetowarzone/MultiplayerFragment.java
@@ -61,4 +61,5 @@ public class MultiplayerFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_multiplayer, container, false);
     }
+
 }
